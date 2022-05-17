64e3a2774166ae41cf150c02e5b43bfe16920cb8
Юрий Данильченко <genitra111@gmail.com>
2022-05-17 16:43:33 +1000

    Внесены правки в код для корректной работы программы

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.function.*;
 
 public class Calculator {
@@ -7,12 +9,21 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
+
+    // если делитель(y) будет равен нулю, то это приведёт к возникновению
+    // ошибки т.к. на ноль делить нельзя
     BinaryOperator<Integer> devide = (x, y) -> x / y;
+
+    // в этом случае ошибка не возникнет, т.к. выполняется проверка делителя(y)
+    // и если делитель равен нулю, то возвращаемым значением будет делимое(x)
+    BinaryOperator<Integer> devide2 = (x, y) -> y == 0 ? x : x / y;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
     Predicate<Integer> isPositive = x -> x > 0;
+
+    Predicate<Integer> isNull = x -> x == 0;
 
     Consumer<Integer> println = System.out::println;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,8 +7,16 @@ public class Main {
 
         int a = calc.plus.apply(1, 3);
         int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b);
+        //int c = calc.devide.apply(a, b);
 
-        calc.println.accept(с);
+        int d = calc.devide2.apply(a, b);
+
+        //вызов этого метода с переменной c приведёт к ошибке, т.к. делить на ноль нельзя
+        //calc.println.accept(с);
+
+        // в этом случае ошибки не будет
+        calc.println.accept(d);
+
+        System.out.println(calc.isNull.test(b) ? "На ноль делить нельзя" : "");
     }
 }
